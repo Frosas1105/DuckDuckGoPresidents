7a936f858c4c970191779a7df107e3782b2f9aac
Frosas1105 <90847745+Frosas1105@users.noreply.github.com>
2021-10-22 12:58:33 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import pytest
 import requests
 url = "https://api.duckduckgo.com/?q=presidents+of+the+united+states&format=json"
+presidents = [" Washington", " Adams", " Jefferson", " Madison", " Monroe", " Adams", " Jackson", " Buren", " Harrison", " Tyler", " Polk", " Taylor", " Fillmore", " Pierce",  " Buchanan", " Lincoln", " Johnson", " Grant", " Hayes", " Garfield", " Arthur", " Cleveland", " Harrison", " Cleveland", " McKinley", " Roosevelt", " Taft",  " Wilson", " Harding", " Coolidge", " Hoover", " Roosevelt", " Truman", " Eisenhower", " Kennedy", " Johnson", " Nixon", " Ford", " Carter", " Reagan", " Bush",  " Clinton", " Obama", " Trump", " Biden"]
+
 def test_DuckDuckGo():
   r = requests.get(url)
   resp_data = resp.json()
